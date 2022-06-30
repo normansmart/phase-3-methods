9aa052d7f18943233ea398b6d70f962015a2303e
Norman Smart <normansmart14@gmail.com>
2022-06-30 12:17:33 -0400

    commit tree e65c8b07d66b70aa6a87f80183f0fc93b13cdf6d

--- methods.rb
+++ methods.rb
@@ -1,1 +1,50 @@
 # Your code here!
+
+
+def greet_programmer
+
+    puts "Hello, programmer!"
+
+end
+
+ greet_programmer
+
+
+
+def greet (name)
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+
+
+
+def greet_with_default( name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default
+greet_with_default("Naureen")
+
+
+def add (num1 , num2)
+  return num1 + num2
+end
+
+
+puts add(1 , 2)
+
+
+def halve (num)
+
+    if num.class != Integer
+        return nil
+    end
+
+    return num/2
+
+end
+
+
+halve(6)
+
+halve("six")
